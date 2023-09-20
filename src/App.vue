3c97ea881a621b7bf6b2bbbc1e3a43e681dbf905
Pan-Pan-Pan <1425650574@qq.com>
2023-09-20 11:20:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="basic-btn">666</button>
+  <br><br>
+  <button class="basic-btn-outline">777</button>
+  <br><br>
+  <input type="text" :value="msg">
+  <br><br>
+  <div class="huge-input-wrapper">
+    <input class="huge-input" type="text" placeholder="文献、期刊、作者以及更多"/>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+let msg = ref('hhh')
+
+</script>
+
+<style scoped>
+</style>
