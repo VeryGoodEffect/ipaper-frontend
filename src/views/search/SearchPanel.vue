06c6a3d489a99d096223013c77085815e14885af
Illli <254906270@qq.com>
2023-12-26 17:50:30 +0800

    解决翻译

--- src/views/search/SearchPanel.vue
+++ src/views/search/SearchPanel.vue
@@ -2,45 +2,22 @@
   <div>
     <div class="search-area">
       <div>
-        <input
-          v-model="search_search"
-          type="text"
-          class="basic-input search-input"
-          :placeholder="place_holder"
-          @keydown.down="navigateDown"
-          @keydown.up="navigateUp"
-          @keydown.enter="searchOrChangeContent"
-          @focus="showAutoCompleteMenu"
-          @blur="hideAutoCompleteMenu"
-        />
+        <input v-model="search_search" type="text" class="basic-input search-input" :placeholder="place_holder"
+          @keydown.down="navigateDown" @keydown.up="navigateUp" @keydown.enter="searchOrChangeContent"
+          @focus="showAutoCompleteMenu" @blur="hideAutoCompleteMenu" />
         <ul v-if="autoCompleteShouldShow && autoCompleteLists.length > 0">
-          <li
-            :class="{ 'suggestion-active': index === activeSuggestionIndex }"
-            v-for="(item, index) in autoCompleteLists"
-            :key="index"
-            @mouseover="activeSuggestionIndex = index"
-            @click="changeContent(item.display_name)"
-          >
+          <li :class="{ 'suggestion-active': index === activeSuggestionIndex }" v-for="(item, index) in autoCompleteLists"
+            :key="index" @mouseover="activeSuggestionIndex = index" @click="changeContent(item.display_name)">
             {{ item.display_name }}
           </li>
         </ul>
       </div>
       <button @click="search" class="basic-btn search-btn">
-        <svg
-          t="1699356103686"
-          class="icon"
-          viewBox="0 0 1024 1024"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          p-id="4162"
-          width="200"
-          height="200"
-        >
+        <svg t="1699356103686" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+          p-id="4162" width="200" height="200">
           <path
             d="M945.066667 898.133333l-189.866667-189.866666c55.466667-64 87.466667-149.333333 87.466667-241.066667 0-204.8-168.533333-373.333333-373.333334-373.333333S96 264.533333 96 469.333333 264.533333 842.666667 469.333333 842.666667c91.733333 0 174.933333-34.133333 241.066667-87.466667l189.866667 189.866667c6.4 6.4 14.933333 8.533333 23.466666 8.533333s17.066667-2.133333 23.466667-8.533333c8.533333-12.8 8.533333-34.133333-2.133333-46.933334zM469.333333 778.666667C298.666667 778.666667 160 640 160 469.333333S298.666667 160 469.333333 160 778.666667 298.666667 778.666667 469.333333 640 778.666667 469.333333 778.666667z"
-            p-id="4163"
-            fill="#fff"
-          ></path>
+            p-id="4163" fill="#fff"></path>
         </svg>
       </button>
     </div>
@@ -73,6 +50,7 @@ export default {
       search_perpage: 10,
       search_page: 1,
       cur_search_cursor: "",
+      place_holder_type: 0,
       search_type: 1,
       queryParts: {},
       autoCompleteLists: [],
@@ -81,7 +59,11 @@ export default {
       place_holder: this.$t("search_type_0"),
     };
   },
-
+  computed: {
+    language() {
+      return this.$t('web_name_text')
+    }
+  },
   watch: {
     search_search(newValue, oldValue) {
       if (newValue.length == 0) {
@@ -92,6 +74,9 @@ export default {
         this.autoComplete();
       }
     },
+    language() {
+      this.place_holder = this.$t(`search_type_${this.place_holder_type}`)
+    }
   },
 
   methods: {
@@ -136,7 +121,7 @@ export default {
     setSearchContent(data) {
       this.search_search = data;
     },
-    buildQuery() {},
+    buildQuery() { },
     advsearch(data) {
       // alert("data sent to advsearch");
       // queryParts = [];
@@ -169,6 +154,7 @@ export default {
     // https://api.openalex.org/authors?filter=display_name.search:tupolev
     // https://api.openalex.org/authors?filter=display_name.search:john%20smith
     setSearchType(type) {
+      this.place_holder_type = type
       if (type == 0) {
         // display_name.search:
         // this.search_filter = "display_name.search:"
